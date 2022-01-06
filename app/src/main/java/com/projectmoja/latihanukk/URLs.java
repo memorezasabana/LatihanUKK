2109f90b8cda48117aff409e94b22ed48e40c827
Memoreza Sabana <53498578+memorezasabana@users.noreply.github.com>
2022-01-06 19:19:30 +0700

    Login

---
+++ app/src/main/java/com/projectmoja/latihanukk/URLs.java
@@ -0,0 +1,8 @@
+package com.projectmoja.latihanukk;
+
+public class URLs {
+    private static final String ROOT_URL = "http://192.168.100.90/login/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
